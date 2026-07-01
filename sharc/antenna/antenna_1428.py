8d0916530248ea83bd2f8766e164ff664932ad32
artistrea <apiravesi@gmail.com>
2026-07-01 17:30:50 -0300

    feat: antenna 1428

---
+++ sharc/antenna/antenna_1428.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+
+def es_ant_gain_1428(off_axis, ant_gain, d_over_lmbda):
+    off_axis = np.atleast_1d(off_axis)
+    g = np.zeros_like(off_axis)
+
+    if d_over_lmbda < 20:
+        raise ValueError("ma burro")
+    elif d_over_lmbda <= 25:
+        g_max = 20*np.log10(d_over_lmbda) + 7.7
+        g1 = 29 - 25 * np.log10(95/d_over_lmbda)
+        phi_m = 20 / d_over_lmbda * np.sqrt(g_max - g1)
+        # unsupported:
+        g[off_axis >= 180.] = -5
+        g[off_axis < 180.] = -5
+        g[off_axis < 80.] = -9
+        g[off_axis < 33.1] = 29 - 25 * np.log10(off_axis[off_axis < 33.1])
+        g[off_axis < 95 / d_over_lmbda] = g1
+        assert phi_m < 95 / d_over_lmbda
+        g[off_axis < phi_m] = ant_gain - 2.5e-3 * (d_over_lmbda*off_axis[off_axis < phi_m])**2
+    elif d_over_lmbda <= 100:
+        g_max = 20*np.log10(d_over_lmbda) + 7.7
+        g1 = 29 - 25 * np.log10(95/d_over_lmbda)
+        phi_m = 20 / d_over_lmbda * np.sqrt(g_max - g1)
+        # unsupported:
+        g[off_axis >= 180.] = -9
+        g[off_axis < 180.] = -9
+        g[off_axis < 120.] = -4
+        g[off_axis < 80.] = -9
+        g[off_axis < 33.1] = 29 - 25 * np.log10(off_axis[off_axis < 33.1])
+        g[off_axis < 95 / d_over_lmbda] = g1
+        assert phi_m < 95 / d_over_lmbda
+        g[off_axis < phi_m] = ant_gain - 2.5e-3 * (d_over_lmbda*off_axis[off_axis < phi_m])**2
+    elif d_over_lmbda > 100:
+        g_max = 20*np.log10(d_over_lmbda) + 8.4
+        g1 = -1 + 15 * np.log10(d_over_lmbda)
+        phi_m = 20 / d_over_lmbda * np.sqrt(g_max - g1)
+        phi_r = 15.85 * d_over_lmbda ** -0.6
+        # unsupported:
+        g[off_axis >= 180.] = -12
+        g[off_axis < 180.] = -12
+        g[off_axis < 120.] = -7
+        g[off_axis < 80.] = -12
+        g[off_axis < 34.1] = 34 - 30 * np.log10(off_axis[off_axis < 34.1])
+        g[off_axis < 10.] = 29 - 25 * np.log10(off_axis[off_axis < 10.])
+        g[off_axis < phi_r] = g1
+        assert phi_r < 10.
+        assert phi_m < phi_r
+        g[off_axis < phi_m] = ant_gain - 2.5e-3 * (d_over_lmbda*off_axis[off_axis < phi_m])**2
+
+    return g
